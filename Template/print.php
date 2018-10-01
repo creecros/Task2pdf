2bad30a53322c992d42fa5951df5fa9e7b871a92
Craig Crosby <creecros@gmail.com>
2018-10-01 12:50:45 -0400

    Update print.php

--- Template/print.php
+++ Template/print.php
@@ -1,7 +1,7 @@
 <?php if ($this->user->hasProjectAccess('metadata', 'index', $task['project_id'])): ?>
     <ul>
         <li>
-        <i class="fa fa-plus-square-o fa-fw"></i>
+        <i class="fa-file-pdf-o"></i>
         <?= $this->url->link(t('Print'), 'PrintTaskController', 'printTask', ['plugin' => 'task2pdf', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>
         </li>
     </ul>
