0cb6cbaba88bdb4d21f0db167d88a7e3659ae60b
Craig Crosby <creecros@gmail.com>
2018-10-01 14:08:56 -0400

    Update PrintTaskController.php

--- Controller/PrintTaskController.php
+++ Controller/PrintTaskController.php
@@ -24,6 +24,7 @@ class PrintTaskController extends BaseController
     {
         // instantiate and use t$options = new Options();
         $options = new Options();
+        $dompdf->set_base_path(realpath(APPLICATION_PATH . '/var/www/app/'));
         $options->set('isRemoteEnabled', 'true');
         $options->set('defaultFont', 'Helvetica');
         $dompdf = new Dompdf($options);
