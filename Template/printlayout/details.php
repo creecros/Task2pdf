afcf4b94cdf34c0773977169f3a53ae9eee7c27c
Craig Crosby <creecros@gmail.com>
2018-10-01 17:05:12 -0400

    Update details.php

--- Template/printlayout/details.php
+++ Template/printlayout/details.php
@@ -1,1 +1,126 @@
 
+<section id="task-summary">
+    <h2><?= $this->text->e($task['title']) ?></h2>
+    <div class="task-summary-container color-<?= $task['color_id'] ?>">
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+        <tr>
+                    <td>
+                        <strong><?= t('Status:') ?></strong>
+                        <span>
+                        <?php if ($task['is_active'] == 1): ?>
+                            <?= t('open') ?>
+                        <?php else: ?>
+                            <?= t('closed') ?>
+                        <?php endif ?>
+                        </span>
+                    </td>
+                    <td>
+                            <strong><?= t('Category:') ?></strong>
+                            <span><?= $this->text->e($task['category_name']) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Assignee:') ?></strong>
+                        <span>
+                        <?php if ($task['assignee_username']): ?>
+                            <?= $this->text->e($task['assignee_name'] ?: $task['assignee_username']) ?>
+                        <?php else: ?>
+                            <?= t('not assigned') ?>
+                        <?php endif ?>
+                        </span>
+                        <?php if ($editable && $task['owner_id'] != $this->user->getId()): ?>
+                            - <span><?= $this->url->link(t('Assign to me'), 'TaskModificationController', 'assignToMe', ['task_id' => $task['id'], 'project_id' => $task['project_id']]) ?></span>
+                        <?php endif ?>
+                    </td>
+                    <td>
+                            <strong><?= t('Due date:') ?></strong>
+                            <span><?= $this->dt->datetime($task['date_due']) ?></span>
+                    </td>
+      </tr>
+      <tr>         
+                    <td>
+                        <strong><?= t('Priority:') ?></strong> <span><?= $task['priority'] ?></span>
+                    </td>
+                    <td>
+                            <strong><?= t('Swimlane:') ?></strong>
+                            <span><?= $this->text->e($task['swimlane_name']) ?></span>
+                    </td>
+                    <td>
+                            <strong><?= t('Creator:') ?></strong>
+                            <span><?= $this->text->e($task['creator_name'] ?: $task['creator_username']) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Started:') ?></strong>
+                        <?php if ($task['date_started']): ?>
+                            <span><?= $this->dt->datetime($task['date_started']) ?></span>
+                        <?php elseif ($editable): ?>
+                            <span><?= $this->url->link(t('Start now'), 'TaskModificationController', 'start', ['task_id' => $task['id'], 'project_id' => $task['project_id']]) ?></span>
+                        <?php endif ?>
+                    </td>   
+      </tr>
+      <tr>
+                    <td>
+                            <strong><?= t('Reference:') ?></strong> <span><?= $this->task->renderReference($task) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Column:') ?></strong>
+                        <span><?= $this->text->e($task['column_title']) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Time estimated:') ?></strong>
+                        <span><?= t('%s hours', $task['time_estimated']) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Created:') ?></strong>
+                        <span><?= $this->dt->datetime($task['date_creation']) ?></span>
+                    </td>
+      </tr>
+      <tr>
+                    <td>
+                            <strong><?= t('Complexity:') ?></strong> <span><?= $this->text->e($task['score']) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Position:') ?></strong>
+                        <span><?= $task['position'] ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Time spent:') ?></strong>
+                        <span><?= t('%s hours', $task['time_spent']) ?></span>
+                    </td>
+                    <td>
+                        <strong><?= t('Modified:') ?></strong>
+                        <span><?= $this->dt->datetime($task['date_modification']) ?></span>
+                    </td>
+      </tr>
+      <tr>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                    <td>
+                        <strong><?= t('Completed:') ?></strong>
+                        <span><?= $this->dt->datetime($task['date_completed']) ?></span>
+                    </td>
+      </tr>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                    <td>
+                    </td>       
+                    <td>
+                        <strong><?= t('Moved:') ?></strong>
+                        <span><?= $this->dt->datetime($task['date_moved']) ?></span>
+                    </td>
+      </tr>
+
+
+  </div>
+
+</section>
