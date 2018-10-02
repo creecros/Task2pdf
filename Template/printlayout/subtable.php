5ca12b6b49182e9982b6b1907f33f554ea8cf5cf
Craig Crosby <creecros@gmail.com>
2018-10-02 10:08:44 -0400

    Update subtable.php

--- Template/printlayout/subtable.php
+++ Template/printlayout/subtable.php
@@ -35,10 +35,6 @@
                    <?php if (! empty($subtask['time_estimated'])): ?>
                    <?= t('%sh estimated', n($subtask['time_estimated'])) ?>
                    <?php endif ?>
-
-                   <?php if ($this->user->hasProjectAccess('SubtaskController', 'edit', $task['project_id']) && $subtask['user_id'] == $this->user->getId()): ?>
-                       <?= $this->subtask->renderTimer($task, $subtask) ?>
-                   <?php endif ?>
                 </span>
             </td>
         </tr>
