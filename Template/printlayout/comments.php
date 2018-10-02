526c5c5f1e520e8666a140c94ae6fdafdf5f8e77
Craig Crosby <creecros@gmail.com>
2018-10-02 00:32:14 -0400

    Update comments.php

--- Template/printlayout/comments.php
+++ Template/printlayout/comments.php
@@ -1,1 +1,17 @@
+<section class="accordion-section <?= empty($comments) ? 'accordion-collapsed' : '' ?>">
+    <div class="accordion-title">
+        <h3><a href="#" class="fa accordion-toggle"></a> <?= t('Comments') ?></h3>
+    </div>
+    <div class="accordion-content comments" id="comments">
+        <?php foreach ($comments as $comment): ?>
+            <?= $this->render('Task2pdf:printlayout/comments_show', array(
+                'comment'   => $comment,
+                'task'      => $task,
+                'project'   => $project,
+                'editable'  => $editable,
+                'is_public' => isset($is_public) && $is_public,
+            )) ?>
+        <?php endforeach ?>
+    </div>
+</section>
 
