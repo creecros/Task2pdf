98a4989f28bc944d10582967170643aa7886b983
Craig Crosby <creecros@gmail.com>
2018-10-01 21:54:44 -0400

    Update subtasks.php

--- Template/printlayout/subtasks.php
+++ Template/printlayout/subtasks.php
@@ -1,1 +1,12 @@
-
+<section class="accordion-section <?= empty($subtasks) ? 'accordion-collapsed' : '' ?>">
+    <div class="accordion-title">
+        <h3><a href="#" class="fa accordion-toggle"></a> <?= t('Sub-Tasks') ?></h3>
+    </div>
+    <div class="accordion-content">
+        <?= $this->render('task2pdf:printlayout/subtable', array(
+            'subtasks' => $subtasks,
+            'task' => $task,
+            'editable' => $editable
+        )) ?>
+    </div>
+</section>
