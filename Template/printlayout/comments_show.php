fc9f8b76a6752a05acfec87fd5af376b7e178e93
Craig Crosby <creecros@gmail.com>
2018-10-02 01:04:57 -0400

    Update comments_show.php

--- Template/printlayout/comments_show.php
+++ Template/printlayout/comments_show.php
@@ -1,16 +1,17 @@
 <div class="comment <?= isset($preview) ? 'comment-preview' : '' ?>" id="comment-<?= $comment['id'] ?>">
+    <div class="comment-content">
+        <div style="background-color:#eee" class="markdown">
+            <?= $this->text->markdown($comment['comment'], isset($is_public) && $is_public) ?>
+        </div>
+    </div>
     <div class="comment-title">
-        <a style=" display: inline-block; font-size:9px; text-align: left; width: 100%;"> 
+        <a style=" display: inline-block; font-size:9px; text-align: right; width: 100%;"> 
+            <?= t('-') ?>
         <?php if (! empty($comment['username'])): ?>
             <strong class="comment-username"><?= $this->text->e($comment['name'] ?: $comment['username']) ?></strong>
         <?php endif ?>
         <small class="comment-date"><?= t('Created at:') ?> <?= $this->dt->datetime($comment['date_creation']) ?></small>
         <small class="comment-date"><?= t('Updated at:') ?> <?= $this->dt->datetime($comment['date_modification']) ?></small>
         </a>
-    </div>
-    <div class="comment-content">
-        <div class="markdown">
-            <?= $this->text->markdown($comment['comment'], isset($is_public) && $is_public) ?>
-        </div>
     </div>
 </div>
