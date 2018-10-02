87ad63016af101904dccc85240b1be9c0c145aa1
Craig Crosby <creecros@gmail.com>
2018-10-02 11:04:56 -0400

    Create files.php

---
+++ Template/printlayout/files.php
@@ -0,0 +1,26 @@
+<?php if (! empty($files)): ?>
+    <table class="table-striped table-scrolling">
+        <tr style="border:1px solid #eee;border-collapse:collapse;width:100%;">
+            <th><?= t('Filename') ?></th>
+            <th><?= t('Creator') ?></th>
+            <th><?= t('Date') ?></th>
+            <th><?= t('Size') ?></th>
+        </tr>
+        <?php foreach ($files as $file): ?>
+            <tr style="background-color: #fff;">
+                <td>
+                    <?= $this->text->e($file['name']) ?>
+                </td>
+                <td>
+                    <?= $this->text->e($file['user_name'] ?: $file['username']) ?>
+                </td>
+                <td>
+                    <?= $this->dt->date($file['date']) ?>
+                </td>
+                <td>
+                    <?= $this->text->bytes($file['size']) ?>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+<?php endif ?>
