060165fd45d8c8dcf9b74595ff2cbc964529e798
Craig Crosby <creecros@gmail.com>
2018-10-01 23:49:20 -0400

    Update subtable.php

--- Template/printlayout/subtable.php
+++ Template/printlayout/subtable.php
@@ -1,7 +1,7 @@
 <?php if (! empty($subtasks)): ?>
-    <table style="border:1px solid #eee;border-collapse:collapse;background-color: #eee;width:100%;">
+    <table style="border:1px solid #eee;border-collapse:collapse;width:100%;">
 
-        <tr style="background-color: #fff;">
+        <tr style="background-color: #eee;">
             <th><?= t('Title') ?></th>
             <th><?= t('Assignee') ?></th>
             <?= $this->hook->render('template:subtask:table:header:before-timetracking') ?>
@@ -11,10 +11,10 @@
         <tr style="background-color: #fff;">
             <td>
                 <?php if ($subtask['status'] > 1): ?>
-                    <?= t('[x] - ') ?>
+                    <?= t('[x] ') ?>
                     <?= $this->subtask->renderTitle($subtask) ?>
                 <?php else: ?>
-                    <?= t('[ ] - ') ?>
+                    <?= t('[ ] ') ?>
                     <?= $this->subtask->renderTitle($subtask) ?>
                 <?php endif ?>
             </td>
