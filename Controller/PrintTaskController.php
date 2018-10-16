4b3225f7a269590e1e25c7984c4a0e2b908b5e72
Craig Crosby <creecros@gmail.com>
2018-10-15 20:41:48 -0400

    Update PrintTaskController.php

--- Controller/PrintTaskController.php
+++ Controller/PrintTaskController.php
@@ -68,13 +68,6 @@ class PrintTaskController extends BaseController
         $dompdf->stream($task['id'] . '_' . $task['title'] . '.pdf');
 
     }
-    
-    public function codeblockFix($toFix)
-    {
-        $toFix = str_replace('```', '`', $toFix);
-        $toFix = str_replace('~~~', '`', $toFix);
-        return $toFix;
-    }
 
 }
 
