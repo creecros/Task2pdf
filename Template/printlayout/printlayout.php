6c63d4f7d900e203544f0840ce74936bf17a0219
Craig Crosby <creecros@gmail.com>
2019-04-11 12:12:51 -0400

    Update printlayout.php

--- Template/printlayout/printlayout.php
+++ Template/printlayout/printlayout.php
@@ -7,20 +7,20 @@
       font-family: 'Firefly Sung';
       font-style: normal;
       font-weight: 400;
-      src: url(https://github.com/creecros/Task2pdf/blob/creecros-patch-2/font/DroidSansFallback.ttf?raw=true) format('truetype');
+      src: url(https://github.com/creecros/Task2pdf/blob/creecros-patch-2/font/NotoSansMonoCJKsc-Regular.otf?raw=true) format('truetype');
     }
     @font-face {
       font-family: 'Firefly Sung';
       font-style: normal;
       font-weight: bold;
-      src: url(https://github.com/creecros/Task2pdf/blob/creecros-patch-2/font/DroidSansFallback.ttf?raw=true) format('truetype');
+      src: url(https://github.com/creecros/Task2pdf/blob/creecros-patch-2/font/NotoSansMonoCJKsc-Regular.otf?raw=true) format('truetype');
     }
      @font-face {
       font-family: 'Firefly Sung';
       font-style: normal;
       font-weight: bold;
       font-style: italic;
-         src: url(https://github.com/creecros/Task2pdf/blob/creecros-patch-2/font/DroidSansFallback.ttf?raw=true) format('truetype');
+         src: url(https://github.com/creecros/Task2pdf/blob/creecros-patch-2/font/NotoSansMonoCJKsc-Regular.otf?raw=true) format('truetype');
     }
     * {
       font-family: Firefly Sung, DejaVu Sans, sans-serif;
