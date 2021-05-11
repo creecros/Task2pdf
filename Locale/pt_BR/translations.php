e97a920255a64aa83e2a80710e0b6b4bd0596eee
Luis <luisfelipeandrade@gmail.com>
2021-05-10 23:41:48 -0300

    Add portuguese brazilian translate

---
+++ Locale/pt_BR/translations.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'Create PDF' => 'Gerar PDF',
+    'Print Open Tasks' => 'Imprimir Tarefas Abertas',
+    'Created at' => 'Criado em',
+    'Updated at' => 'Atualizado em',
+    'Font Options' => 'Opções de Fontes',
+    'Embed Task Files Options' => 'Opções de incorporação de arquivos de tarefas',
+    'Download de PDF ou Inline' => 'Download de PDF ou exibição no navegador',
+    'Normal Font set when creating PDFs' => 'Fonte normal definida ao criar PDFs',
+    'CJK Font set when creating PDFs' => 'Fonte CJK definida ao criar PDFs',
+    'Embed Files when creating a PDF for a Task' => 'Incorporar arquivos ao criar um PDF para uma tarefa',
+    'Do not embed Files when creating a PDF for a Task' => 'Não incorpore arquivos ao criar um PDF para uma tarefa',
+    'Embed Files when creating a PDF for all Open Tasks in a Project' => 'Incorporar arquivos ao criar um PDF para todas as tarefas abertas em um projeto',
+    'Do Not Embed Files when creating a PDF for all Open Tasks in a Project' => 'Não incorpore arquivos ao criar um PDF para todas as tarefas abertas em um projeto',
+    'Show PDF inline Browser' => 'Mostrar pdf no navegador',
+    'Download pdf' => 'Baixar pdf'
+);
--- Template/config/font_toggle.php
+++ Template/config/font_toggle.php
@@ -1,16 +1,17 @@
 <div class="panel"><h1>Task2PDF Options</h1>
     <br>
-    <h2>Font Options:</h2>
-    <?= $this->form->radio('task2pdf_cjk', 'Normal Font set when creating PDFs' , 1, isset($values['task2pdf_cjk'])&& $values['task2pdf_cjk']==1) ?>
-    <?= $this->form->radio('task2pdf_cjk', 'CJK Font set when creating PDFs' , 2, isset($values['task2pdf_cjk'])&& $values['task2pdf_cjk']==2) ?>
+    <h2><?= t('Font Options') ?>:</h2>
+    <?= $this->form->radio('task2pdf_cjk', t('Normal Font set when creating PDFs') , 1, isset($values['task2pdf_cjk'])&& $values['task2pdf_cjk']==1) ?>
+    <?= $this->form->radio('task2pdf_cjk', t('CJK Font set when creating PDFs') , 2, isset($values['task2pdf_cjk'])&& $values['task2pdf_cjk']==2) ?>
     <br>
-    <h2>Embed Task Files Options:</h2>
-	<?= $this->form->radio('task2pdf_embed_task', 'Embed Files when creating a PDF for a Task' , 1, isset($values['task2pdf_embed_task'])&& $values['task2pdf_embed_task']==1) ?>
-    <?= $this->form->radio('task2pdf_embed_task', 'Do not embed Files when creating a PDF for a Task' , 2, isset($values['task2pdf_embed_task'])&& $values['task2pdf_embed_task']==2) ?>
-    <?= $this->form->radio('task2pdf_embed_projects', 'Embed Files when creating a PDF for all Open Tasks in a Project' , 1, isset($values['task2pdf_embed_projects'])&& $values['task2pdf_embed_projects']==1) ?>
-    <?= $this->form->radio('task2pdf_embed_projects', 'Do Not Embed Files when creating a PDF for all Open Tasks in a Project' , 2, isset($values['task2pdf_embed_projects'])&& $values['task2pdf_embed_projects']==2) ?>
+    <h2><?= t('Embed Task Files Options') ?>:</h2>
+	<?= $this->form->radio('task2pdf_embed_task', t('Embed Files when creating a PDF for a Task') , 1, isset($values['task2pdf_embed_task'])&& $values['task2pdf_embed_task']==1) ?>
+    <?= $this->form->radio('task2pdf_embed_task', t('Do not embed Files when creating a PDF for a Task') , 2, isset($values['task2pdf_embed_task'])&& $values['task2pdf_embed_task']==2) ?>
+    <?= $this->form->radio('task2pdf_embed_projects', t('Embed Files when creating a PDF for all Open Tasks in a Project') , 1, isset($values['task2pdf_embed_projects'])&& $values['task2pdf_embed_projects']==1) ?>
+    <?= $this->form->radio('task2pdf_embed_projects', t('Do Not Embed Files when creating a PDF for all Open Tasks in a Project') , 2, isset($values['task2pdf_embed_projects'])&& $values['task2pdf_embed_projects']==2) ?>
     <br>
-    <h2>PDF Download or Inline:</h2>
-	<?= $this->form->radio('task2pdf_attachment', 'Show PDF inline Browser' , 1, isset($values['task2pdf_attachment'])&& $values['task2pdf_attachment']==1) ?>
-    <?= $this->form->radio('task2pdf_attachment', 'Download pdf' , 2, isset($values['task2pdf_attachment'])&& $values['task2pdf_attachment']==2) ?>
+    <h2><?= t('PDF Download or Inline') ?>:</h2>
+	<?= $this->form->radio('task2pdf_attachment', t('Show PDF inline Browser') , 1, isset($values['task2pdf_attachment'])&& $values['task2pdf_attachment']==1) ?>
+    <?= $this->form->radio('task2pdf_attachment', t('Download pdf') , 2, isset($values['task2pdf_attachment'])&& $values['task2pdf_attachment']==2) ?>
+    <?= $this->form->radio('task2pdf_attachment', t('Download pdf') , 2, isset($values['task2pdf_attachment'])&& $values['task2pdf_attachment']==2) ?>
 </div>
