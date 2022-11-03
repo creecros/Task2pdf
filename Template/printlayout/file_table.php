9957a488196b99a21b4a5b397d8ad3a864dd9820
Stehle <29704323+stehlegg@users.noreply.github.com>
2022-11-03 17:54:07 +0100

    Add files via upload

--- Template/printlayout/file_table.php
+++ Template/printlayout/file_table.php
@@ -23,11 +23,21 @@
             </tr>
         <?php endforeach ?>
         <?php foreach ($images as $image): ?>
-                    <?= $file_count = count($images); ?>
-                    <?= $width = 100/$file_count; ?>
+            <tr style="background-color: #fff;">
+                <td>
                     <img src="data:image/png;base64,<?= base64_encode(file_get_contents(FILES_DIR.DIRECTORY_SEPARATOR.$image['path'])); ?>" 
-                        style="width: <?= $width; ?>%;border:1px solid #efefef;border-radius:5px;margin-bottom:20px;box-shadow:4px 2px 10px -6px rgb(0 0 0 / 55%);margin-right: 15px">
-
+                        style="width: 100%;border:1px solid #efefef;border-radius:5px;margin-bottom:20px;box-shadow:4px 2px 10px -6px rgb(0 0 0 / 55%);margin-right: 15px">
+                </td>
+                <td>
+                    <?= $this->text->e($image['user_name'] ?: $image['username']) ?>
+                </td>
+                <td>
+                    <?= $this->dt->date($image['date']) ?>
+                </td>
+                <td>
+                    <?= $this->text->bytes($image['size']) ?>
+                </td>
+            </tr>
         <?php endforeach ?>
     </table>
 <?php endif ?>
