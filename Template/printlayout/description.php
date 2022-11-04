860eec0d54f2c65e3fc3a1473648e161f21864a7
Stehle <29704323+stehlegg@users.noreply.github.com>
2022-11-04 01:28:27 +0100

    Update description.php

--- Template/printlayout/description.php
+++ Template/printlayout/description.php
@@ -2,6 +2,12 @@
     <div class="accordion-title">
         <h3><a href="#" class="fa accordion-toggle"></a> <?= t('Description') ?></h3>
     </div>
+    <style>
+	img {
+		max-width:100%;
+		max-height:100%;
+	}
+    </style>
     <div class="accordion-content">
         <article class="markdown">
             <?= $this->text->markdown($this->task->printModel->codeblockFix($task['description']), isset($is_public) && $is_public) ?>
