d164eab4b800f3797a18bff15d1a75fe5d81d75c
aljawaid <jawaid_imran@hotmail.com>
2022-11-04 02:14:04 +0000

    Try to align pasted images

--- Template/printlayout/description.php
+++ Template/printlayout/description.php
@@ -2,6 +2,15 @@
     <div class="accordion-title">
         <h3><a href="#" class="fa accordion-toggle"></a> <?= t('Description') ?></h3>
     </div>
+    <style type="text/css">
+        img.enlargable {
+            max-width: 300px;
+            max-height: 300px;
+            cursor: zoom-in;
+            margin: 5px 10px;
+            display: inline-block !important;
+        }
+    </style>
     <div class="accordion-content">
         <article class="markdown">
             <?= $this->text->markdown($this->task->printModel->codeblockFix($task['description']), isset($is_public) && $is_public) ?>
