28711f7d7b44450ffd4abe44cbb804c0eb670c1c
aljawaid <jawaid_imran@hotmail.com>
2022-11-04 03:15:23 +0000

    Fix for images in description in pdf

--- Template/printlayout/description.php
+++ Template/printlayout/description.php
@@ -1,16 +1,26 @@
+<style type="text/css">
+    img.enlargable {
+        max-width: 300px;
+        max-height: 300px;
+        cursor: zoom-in;
+        margin: 20px 10px 7px 10px;
+        /*display: inline-block !important;*/
+    }
+    .markdown p {
+        margin-bottom: 60px;
+    }
+    .accordion-content {
+        margin-top: 0px;
+        margin-bottom: 0px;
+    }
+    .accordion-title h3 {
+        margin-bottom: 0;
+    }
+</style>
 <section class="accordion-section <?= empty($task['description']) ? 'accordion-collapsed' : '' ?>">
     <div class="accordion-title">
         <h3><a href="#" class="fa accordion-toggle"></a> <?= t('Description') ?></h3>
     </div>
-    <style type="text/css">
-        img.enlargable {
-            max-width: 300px;
-            max-height: 300px;
-            cursor: zoom-in;
-            margin: 5px 10px;
-            display: inline-block !important;
-        }
-    </style>
     <div class="accordion-content">
         <article class="markdown">
             <?= $this->text->markdown($this->task->printModel->codeblockFix($task['description']), isset($is_public) && $is_public) ?>
