b9a0a9a12f041eafc2e3ef8a319c3bd0b7e00f64
Kurniawan Jatmika <munirjb@gmail.com>
2023-01-29 18:17:21 +0700

    split export functionality to get all, open & closed tasks lists per current user

--- Template/project_header/print_project.php
+++ Template/project_header/print_project.php
@@ -1,5 +1,11 @@
         <?php if ($this->user->hasProjectAccess('ExportController', 'tasks', $project['id'])): ?>
             <li>
-                <?= $this->url->icon('file-pdf-o', t('Print Open Tasks'), 'PrintTaskController', 'printProject', ['plugin' => 'task2pdf', 'project_id' => $project['id']]) ?>
+                <?= $this->url->icon('file-pdf-o', t('Print All Tasks for This User'), 'PrintTaskController', 'printProjectPerUser', ['plugin' => 'task2pdf', 'project_id' => $project['id']]) ?>
+            </li>
+            <li>
+                <?= $this->url->icon('file-pdf-o', t('Print Open Tasks for This User'), 'PrintTaskController', 'printProjectPerUserOpen', ['plugin' => 'task2pdf', 'project_id' => $project['id']]) ?>
+            </li>
+            <li>
+                <?= $this->url->icon('file-pdf-o', t('Print Closed Tasks for This User'), 'PrintTaskController', 'printProjectPerUserClosed', ['plugin' => 'task2pdf', 'project_id' => $project['id']]) ?>
             </li>
         <?php endif ?>
