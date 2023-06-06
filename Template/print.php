5c3c022055b929ad936c58648b0a1a2befdb243f
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-06 06:53:09 +0100

    FIX: Menu Item is Already Part of List
    - `ul` was being nested unnecessarily and affecting the layout for other hook users

--- Template/print.php
+++ Template/print.php
@@ -1,8 +1,6 @@
 <?php if ($this->user->hasProjectAccess('metadata', 'index', $task['project_id'])): ?>
-    <ul>
-        <li>
+    <li class="pdf-menu-item">
         <i class="fa fa-file-pdf-o fa-fw"></i>
         <?= $this->url->link(t('Create PDF'), 'PrintTaskController', 'printTask', ['plugin' => 'task2pdf', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>
-        </li>
-    </ul>
+    </li>
 <?php endif ?>
